ed4ccf72616ecde82ad70bfdbe7ee332b419e141
Raul Goes <raul.goes-ext@ab-inbev.com>
2021-10-20 07:01:49 -0300

    Add files to project

---
+++ src/main/java/guru/spring/framework/dependencyinjection/services/LifeCycleDemoBean.java
@@ -0,0 +1,41 @@
+package guru.spring.framework.dependencyinjection.services;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware,
+        ApplicationContextAware {
+
+    public LifeCycleDemoBean() {
+        System.out.println("## I'm in the lifecycle constructor");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+
+    }
+
+    @Override
+    public void setBeanName(String s) {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
+}
